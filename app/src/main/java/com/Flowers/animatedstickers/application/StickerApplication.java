a1a18e49b52c67f444961aba7c51aa22a872a1a7
youssefjoundi <yojoundi@student.1337.ma>
2022-08-30 10:39:11 +0100

    Firebase FCM and onesignal notification

--- app/src/main/java/com/Flowers/animatedstickers/application/StickerApplication.java
+++ app/src/main/java/com/Flowers/animatedstickers/application/StickerApplication.java
@@ -14,6 +14,8 @@ import android.app.Application;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.onesignal.OneSignal;
 
 public class StickerApplication extends Application {
     private static final String ONESIGNAL_APP_ID = "b0d04b5c-2088-4ab3-8ef2-9ecd8b149432";
@@ -25,6 +27,19 @@ public class StickerApplication extends Application {
         Fresco.initialize(this);
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+        // promptForPushNotifications will show the native Android notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+
+        FirebaseMessaging.getInstance().subscribeToTopic("sticker_notification");
+
 
     }
 
